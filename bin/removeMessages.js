56e9144cbf415eb2f48a6ea3ea7f8e80c1329abd
Justin J. Novack <jnovack@parxcasino.com>
2016-02-03 14:29:34 -0500

    add sample replay and sample bin file to remove messages

---
+++ bin/removeMessages.js
@@ -0,0 +1,13 @@
+var storm = require('../');
+
+var bool = storm.removeMessages('example.StormReplay');
+
+console.log("replay.messages.events", (bool) ? "successfully" : "NOT", "removed.")
+
+// (with messages)
+//  MD5  = 1ce78a9a6f9b63674c6dfe51afd34456
+//  Size = 549635
+
+// (without messages)
+//  MD5  = f2b50b6c994970dc0b5c789715a20b3c
+//  Size = 549577
